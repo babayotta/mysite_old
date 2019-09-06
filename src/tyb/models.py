f3c3df396202ee2cea30e3719820a44b4222db1e
babayotta <babayotta@protonmail.ch>
2019-09-06 16:25:26 +0300

    transaction type

--- src/tyb/models.py
+++ src/tyb/models.py
@@ -3,9 +3,19 @@ from django.conf import settings
 
 
 class Transaction(models.Model):
+    PROFIT = 'P'
+    TAX = 'T'
+    BUY = 'B'
+    TRANSACTION_TYPES = (
+        (PROFIT, 'Profit'),
+        (TAX, 'Tax'),
+        (BUY, 'Buy'),
+    )
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
                              on_delete=models.CASCADE)
     date = models.DateTimeField()
     description = models.CharField(max_length=300)
     cash = models.FloatField()
-    trans_type = models.BooleanField()
+    transaction_type = models.CharField(max_length=1,
+                                        choices=TRANSACTION_TYPES,
+                                        default=BUY)
