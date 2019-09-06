928bf69579cba9b822b94ddc1aecf0162d160a73
babayotta <babayotta@protonmail.ch>
2019-09-06 13:31:01 +0300

    Some changes

--- src/tyb/models.py
+++ src/tyb/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 
 class Transaction(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     date = models.DateTimeField()
     description = models.CharField(max_length=300)
-    amount = models.FloatField()
+    cash = models.FloatField()
     trans_type = models.BooleanField()
