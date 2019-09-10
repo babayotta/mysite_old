ec5b53a5f45da3a1fda31a52e0563914e9c86bc2
babayotta <babayotta@protonmail.ch>
2019-09-10 15:35:12 +0300

    Updates for the current_month view

--- src/tyb/views.py
+++ src/tyb/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from tyb.models import Transaction
 
 
 def current_month(request):
-    return HttpResponse('Hello world!')
+    if not request.user.is_authenticated:
+        return render(request, 'home.html')
+    transactions = Transaction.user_transactions.current_month(request.user)
+    context = {'transactions': transactions}
+    return render(request, 'tyb/current_month.html', context)
