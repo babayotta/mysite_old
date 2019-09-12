a530337c0d48e005c6947e94623db65b2b44dbe7
babayotta <babayotta@protonmail.ch>
2019-09-12 13:08:29 +0300

    Tuned static, added a base template for the tyb, added a new template for current_month and added css and js for it

--- src/tyb/views.py
+++ src/tyb/views.py
@@ -1,3 +1,4 @@
+import datetime
 from django.shortcuts import render
 from django.http import HttpResponse
 from tyb.models import Transaction
@@ -5,7 +6,7 @@ from tyb.models import Transaction
 
 def current_month(request):
     if not request.user.is_authenticated:
-        return render(request, 'home.html')
+        return render(request, 'mysite/home.html')
     transactions = Transaction.user_transactions.current_month(request.user)
     context = {'transactions': transactions}
     return render(request, 'tyb/current_month.html', context)
