8a0b693221498291e337b304ed120101969cfde0
babayotta <babayotta@protonmail.ch>
2019-09-13 09:29:37 +0300

    Show the sum of buys, profits and taxes on the current_month template

--- src/tyb/views.py
+++ src/tyb/views.py
@@ -7,6 +7,21 @@ def current_month(request):
     if not request.user.is_authenticated:
         return render(request, 'mysite/home.html')
     today = datetime.date.today()
-    transactions = Transaction.user_transactions.get_transactions_for_month(request.user, today.month)
-    context = {'transactions': transactions}
+    user = request.user
+    transactions = Transaction.user_transactions.get_transactions_for_month(user, today)
+    buys_sum = Transaction.user_transactions.get_sum_of_transactions_by_type_for_month(
+        user, today, Transaction.BUY
+    )
+    taxes_sum = Transaction.user_transactions.get_sum_of_transactions_by_type_for_month(
+        user, today, Transaction.TAX
+    )
+    profits_sum = Transaction.user_transactions.get_sum_of_transactions_by_type_for_month(
+        user, today, Transaction.PROFIT
+    )
+    context = {
+        'transactions': transactions,
+        'buys_sum': buys_sum['cash__sum'],
+        'taxes_sum': taxes_sum['cash__sum'],
+        'profits_sum': profits_sum['cash__sum'],
+    }
     return render(request, 'tyb/current_month.html', context)
