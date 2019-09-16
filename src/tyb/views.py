e229e9d452ba92cacce9b6228526f636be9193b3
babayotta <babayotta@protonmail.ch>
2019-09-16 17:33:52 +0300

    Show buys by dates

--- src/tyb/views.py
+++ src/tyb/views.py
@@ -1,6 +1,15 @@
 import datetime
+from calendar import  monthrange
 from django.shortcuts import render
+from django.db.models import Sum
 from tyb.models import Transaction
+
+
+class Entry:
+    def __init__(self, date, transactions, transactions_sum):
+        self.date = date
+        self.transactions = transactions
+        self.transactions_sum = transactions_sum
 
 
 def current_month(request):
@@ -8,18 +17,25 @@ def current_month(request):
         return render(request, 'mysite/home.html')
     today = datetime.date.today()
     user = request.user
-    transactions = Transaction.user_transactions.get_transactions_for_month(user, today)
-    buys_sum = Transaction.user_transactions.get_sum_of_transactions_by_type_for_month(
-        user, today, Transaction.BUY
-    )
-    taxes_sum = Transaction.user_transactions.get_sum_of_transactions_by_type_for_month(
-        user, today, Transaction.TAX
-    )
-    profits_sum = Transaction.user_transactions.get_sum_of_transactions_by_type_for_month(
-        user, today, Transaction.PROFIT
-    )
+    _, number_of_days = monthrange(today.year, today.month)
+
+    transactions = Transaction.objects.filter(user=user, date__year=today.year, date__month=today.month)
+
+    buys = []
+    for date in [datetime.date(today.year, today.month, day) for day in range(1, number_of_days+1)]:
+        print(transactions.filter(transaction_type=Transaction.BUY, date=date).aggregate(Sum('cash'))['cash__sum'])
+        buys.append(Entry(
+            date,
+            transactions.filter(transaction_type=Transaction.BUY, date=date),
+            transactions.filter(transaction_type=Transaction.BUY, date=date).aggregate(Sum('cash'))['cash__sum']
+        ))
+
+    buys_sum = transactions.filter(transaction_type=Transaction.BUY).aggregate(Sum('cash'))
+    taxes_sum = transactions.filter(transaction_type=Transaction.TAX).aggregate(Sum('cash'))
+    profits_sum = transactions.filter(transaction_type=Transaction.PROFIT).aggregate(Sum('cash'))
+
     context = {
-        'transactions': transactions,
+        'buys': buys,
         'buys_sum': buys_sum['cash__sum'],
         'taxes_sum': taxes_sum['cash__sum'],
         'profits_sum': profits_sum['cash__sum'],
