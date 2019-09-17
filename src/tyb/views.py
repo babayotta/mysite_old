bf1a99a532fa28f31fc07a5ccba21894e61a8939
babayotta <babayotta@protonmail.ch>
2019-09-17 08:10:30 +0300

    Show taxes and profits

--- src/tyb/views.py
+++ src/tyb/views.py
@@ -22,22 +22,43 @@ def current_month(request):
     transactions = Transaction.objects.filter(user=user, date__year=today.year, date__month=today.month)
 
     buys = []
+    buy_transactions = transactions.filter(transaction_type=Transaction.BUY)
     for date in [datetime.date(today.year, today.month, day) for day in range(1, number_of_days+1)]:
-        print(transactions.filter(transaction_type=Transaction.BUY, date=date).aggregate(Sum('cash'))['cash__sum'])
+        buy_transactions_for_date = buy_transactions.filter(date=date)
         buys.append(Entry(
             date,
-            transactions.filter(transaction_type=Transaction.BUY, date=date),
-            transactions.filter(transaction_type=Transaction.BUY, date=date).aggregate(Sum('cash'))['cash__sum']
+            buy_transactions_for_date,
+            buy_transactions_for_date.aggregate(Sum('cash'))['cash__sum']
         ))
 
-    buys_sum = transactions.filter(transaction_type=Transaction.BUY).aggregate(Sum('cash'))
-    taxes_sum = transactions.filter(transaction_type=Transaction.TAX).aggregate(Sum('cash'))
-    profits_sum = transactions.filter(transaction_type=Transaction.PROFIT).aggregate(Sum('cash'))
+    taxes = []
+    tax_transactions = transactions.filter(transaction_type=Transaction.TAX)
+    for date in [query.date for query in tax_transactions.order_by('date').distinct('date')]:
+        taxes.append(Entry(
+            date,
+            tax_transactions.filter(date=date),
+            tax_transactions.filter(date=date).aggregate(Sum('cash'))['cash__sum']
+        ))
+
+    profits = []
+    profit_transactions = transactions.filter(transaction_type=Transaction.PROFIT)
+    for date in [query.date for query in profit_transactions.order_by('date').distinct('date')]:
+        profits.append(Entry(
+            date,
+            profit_transactions.filter(date=date),
+            profit_transactions.filter(date=date).aggregate(Sum('cash'))['cash__sum']
+        ))
+
+    buys_sum = buy_transactions.aggregate(Sum('cash'))['cash__sum']
+    taxes_sum = tax_transactions.aggregate(Sum('cash'))['cash__sum']
+    profits_sum = profit_transactions.aggregate(Sum('cash'))['cash__sum']
 
     context = {
         'buys': buys,
-        'buys_sum': buys_sum['cash__sum'],
-        'taxes_sum': taxes_sum['cash__sum'],
-        'profits_sum': profits_sum['cash__sum'],
+        'taxes': taxes,
+        'profits': profits,
+        'buys_sum': buys_sum,
+        'taxes_sum': taxes_sum,
+        'profits_sum': profits_sum,
     }
     return render(request, 'tyb/current_month.html', context)
