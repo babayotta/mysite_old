dbfd8c068b90ed785e6b8c5851578589c4ced33d
babayotta <babayotta@protonmail.ch>
2019-09-17 09:15:47 +0300

    Added collums for budget_for_day and balance

--- src/tyb/views.py
+++ src/tyb/views.py
@@ -12,6 +12,15 @@ class Entry:
         self.transactions_sum = transactions_sum
 
 
+class EntryBuy:
+    def __init__(self, date, transactions, transactions_sum, budget_for_day):
+        self.date = date
+        self.transactions = transactions
+        self.transactions_sum = transactions_sum
+        self.budget_for_day = round(budget_for_day, 2)
+        self.balance = round(budget_for_day - transactions_sum, 2)
+
+
 def current_month(request):
     if not request.user.is_authenticated:
         return render(request, 'mysite/home.html')
@@ -21,39 +30,46 @@ def current_month(request):
 
     transactions = Transaction.objects.filter(user=user, date__year=today.year, date__month=today.month)
 
-    buys = []
     buy_transactions = transactions.filter(transaction_type=Transaction.BUY)
+    tax_transactions = transactions.filter(transaction_type=Transaction.TAX)
+    profit_transactions = transactions.filter(transaction_type=Transaction.PROFIT)
+
+    buys_sum = buy_transactions.aggregate(Sum('cash'))['cash__sum']
+    taxes_sum = tax_transactions.aggregate(Sum('cash'))['cash__sum']
+    profits_sum = profit_transactions.aggregate(Sum('cash'))['cash__sum']
+
+    budget_for_day = ( profits_sum - taxes_sum ) / number_of_days
+
+    buys = []
     for date in [datetime.date(today.year, today.month, day) for day in range(1, number_of_days+1)]:
         buy_transactions_for_date = buy_transactions.filter(date=date)
-        buys.append(Entry(
+        transactions_sum = buy_transactions_for_date.aggregate(Sum('cash'))['cash__sum']
+        buys.append(EntryBuy(
             date,
             buy_transactions_for_date,
-            buy_transactions_for_date.aggregate(Sum('cash'))['cash__sum']
+            transactions_sum if type(transactions_sum) == type(0.1) else 0,
+            budget_for_day,
         ))
 
     taxes = []
-    tax_transactions = transactions.filter(transaction_type=Transaction.TAX)
     for date in [query.date for query in tax_transactions.order_by('date').distinct('date')]:
         tax_transactions_for_date = tax_transactions.filter(date=date)
         taxes.append(Entry(
             date,
             tax_transactions_for_date,
-            tax_transactions_for_date.aggregate(Sum('cash'))['cash__sum']
+            tax_transactions_for_date.aggregate(Sum('cash'))['cash__sum'],
         ))
 
     profits = []
-    profit_transactions = transactions.filter(transaction_type=Transaction.PROFIT)
     for date in [query.date for query in profit_transactions.order_by('date').distinct('date')]:
         profit_transactions_for_date = profit_transactions.filter(date=date)
         profits.append(Entry(
             date,
             profit_transactions_for_date,
-            profit_transactions_for_date.aggregate(Sum('cash'))['cash__sum']
+            profit_transactions_for_date.aggregate(Sum('cash'))['cash__sum'],
         ))
 
-    buys_sum = buy_transactions.aggregate(Sum('cash'))['cash__sum']
-    taxes_sum = tax_transactions.aggregate(Sum('cash'))['cash__sum']
-    profits_sum = profit_transactions.aggregate(Sum('cash'))['cash__sum']
+
 
     context = {
         'buys': buys,
