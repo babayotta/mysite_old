00bf7ca1fa3a06423af12c5cb9d24fe7b94f3f93
babayotta <babayotta@protonmail.ch>
2019-10-25 14:33:37 +0300

    django work, uwsgi work, nginx not

--- src/run.sh
+++ src/run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 python manage.py migrate
-python manage.py runserver 0.0.0.0:8000
+
+#python manage.py runserver 0.0.0.0:8000
+uwsgi --ini uwsgi.ini
+#uwsgi --http :8000 --wsgi-file mysite/wsgi.py
 
