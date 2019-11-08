f9bb2db1c38d7838d67ff6c40602b26a11fdc5b1
babayotta <babayotta@protonmail.ch>
2019-11-08 14:14:31 +0300

    WIP: Vue.js example + trym

---
+++ src/trym/static/trym/api_vue.js
@@ -0,0 +1,85 @@
+Vue.http.headers.common['X-CSRFToken'] = "{{ csrf_token }}";
+new Vue({
+    el: '#starting',
+    delimiters: ['${','}'],
+    data: {
+        transactions: [],
+        loading: true,
+        currentTransaction: {},
+        message: null,
+        newTransaction: {},
+        search_term: '',
+    },
+    mounted: function() {
+        this.getTransactions();
+    },
+    methods: {
+        getTransactions: function() {
+            let api_url = '/api/transaction/';
+            if(this.search_term!==''||this.search_term!==null) {
+                api_url = `/api/transaction/?search=${this.search_term}`
+            }
+            this.loading = true;
+            this.$http.get(api_url)
+                .then((response) => {
+                    this.transactions = response.data;
+                    this.loading = false;
+                })
+                .catch((err) => {
+                    this.loading = false;
+                    console.log(err);
+                })
+        },
+        getTransaction: function(id) {
+            this.loading = true;
+            this.$http.get(`/api/transaction/${id}/`)
+                .then((response) => {
+                    this.currentTransaction = response.data;
+                    $("#editTransactionModal").modal('show');
+                    this.loading = false;
+                })
+                .catch((err) => {
+                    this.loading = false;
+                    console.log(err);
+                })
+        },
+        addTransaction: function() {
+            this.loading = true;
+            this.$http.post('/api/transaction/',this.newTransaction)
+                .then((response) => {
+                    this.loading = true;
+                    this.getTransactions();
+                })
+                .catch((err) => {
+                    this.loading = true;
+                    console.log(err);
+                })
+        },
+        update: function() {
+            this.loading = true;
+            this.$http.put(`/api/transaction/${this.currentTransaction.id}/`, this.currentTransaction)
+                .then((response) => {
+                    this.loading = false;
+                    this.currentTransaction = response.data;
+                    this.getTransactions();
+                })
+                .catch((err) => {
+                    this.loading = false;
+                    console.log(err);
+                })
+        },
+        deleteTransaction: function(id) {
+            this.loading = true;
+            this.$http.delete(`/api/transaction/${id}/`)
+                .then((response) => {
+                    this.loading = false;
+                    this.getTransactions();
+                })
+                .catch((err) => {
+                    this.loading = false;
+                    console.log(err);
+                })
+        }
+    }
+});
+ 
